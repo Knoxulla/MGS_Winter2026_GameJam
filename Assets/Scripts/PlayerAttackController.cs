780f21a070f12a9f7ad7c0012e2b16654c133874
Knox <87248210+Knoxulla@users.noreply.github.com>
2026-02-14 17:47:17 -0500

    Added charge mechanic
    > still need to add projectiles and aiming

--- Assets/Scripts/PlayerAttackController.cs
+++ Assets/Scripts/PlayerAttackController.cs
@@ -1,16 +1,63 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.UI;
 
 public class PlayerAttackController : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [SerializeField] float currentCharge;
+    [SerializeField] float maxChargeAmt;
+    [SerializeField] float chargeAddedPerFrame;
+    [SerializeField] float chargeRemovedPerFrame;
+    [SerializeField] float chargeDmgMultiplier;
+
+    [SerializeField] Image chargeIndicator;
+
+    bool attackIsHeld = false;
+
+    public void OnAttack(InputAction.CallbackContext ctx)
     {
-        
+        if (ctx.started)
+        {
+            attackIsHeld = true;
+        }
+        else if(ctx.canceled)
+        {
+            attackIsHeld = false;
+            chargeDmgMultiplier = 0;
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void FixedUpdate()
     {
-        
+        if (attackIsHeld)
+        {
+            currentCharge += chargeAddedPerFrame;
+            currentCharge = Mathf.Clamp(currentCharge, 0, maxChargeAmt);
+        }
+        else
+        {
+            currentCharge -= chargeRemovedPerFrame;
+            currentCharge = Mathf.Clamp(currentCharge, 0, maxChargeAmt);
+        }
+
+        UpdateChargeIndicator();
+        CalculateDmgMultiplier();
+    }
+
+    private void CalculateDmgMultiplier()
+    {
+        if (currentCharge == maxChargeAmt || !attackIsHeld) return;
+
+        chargeDmgMultiplier += currentCharge;
+    }
+
+    private void UpdateChargeIndicator()
+    { 
+        chargeIndicator.fillAmount = currentCharge / maxChargeAmt;
+
+        if (currentCharge == maxChargeAmt)
+        { 
+            // do special indicator/sound!
+        }
     }
 }
